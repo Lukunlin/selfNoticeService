2c5edb202a850f032e5380df4c8d5d6fd9d6559b
bboykungu-black <727759035@qq.com>
2021-12-15 02:02:19 +0800

    commit: change response for win

--- src/business/czbGitNotice/czbGitNotice.controller.ts
+++ src/business/czbGitNotice/czbGitNotice.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, All } from "@nestjs/common"
+import { Controller, Get, Query, Post, Body } from "@nestjs/common"
 import { ApiBody, ApiTags, ApiOperation, ApiParam } from "@nestjs/swagger"
 import { CzbGitNoticeService } from "./czbGitNotice.service"
 
@@ -6,10 +6,10 @@ import { CzbGitNoticeService } from "./czbGitNotice.service"
 export class CzbGitNoticeController {
 	constructor(private readonly appService: CzbGitNoticeService) {}
 
-	@All()
+	@Post()
 	@ApiTags("czb项目推送服务")
 	@ApiOperation({ summary: "czb项目推送服务", description: "用户对czb项目内的Gitlab触发了保护分支的推送" })
-	pushNotice(@Query() parems: any = {}) {
+	pushNotice(@Body() parems: any = {}) {
 		return parems
 		// return this.appService.pushNotice()
 	}
