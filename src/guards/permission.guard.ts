177d9e9d6efda7a80999390f0d9a2cdae1ea9032
bboykungu <bboykungu@qq.com>
2021-12-28 00:33:27 +0800

    commit: 对于被守卫拦截下来的记录下日志

--- src/guards/permission.guard.ts
+++ src/guards/permission.guard.ts
@@ -1,4 +1,9 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
+import { IncomingMessage } from "http"
+import { LoggerService } from "../logger/logger.service"
+
+// 实例化一些场外对象
+const Logger = new LoggerService()
 
 /**
  * 强制校验SECRET的有效性
@@ -8,11 +13,17 @@ export class checkSecretGuard implements CanActivate {
 	constructor() {}
 
 	async canActivate(context: ExecutionContext): Promise<boolean> {
-		const request = context.switchToHttp().getRequest()
+		const request = context.switchToHttp().getRequest<IncomingMessage>()
 		const header = request.headers || {}
+		const userAgent = header["user-agent"] || header["User-Agent"] || header.userAgent
 		const SECRET = header["x-gitlab-token"]
-		console.log(header)
-		console.log(`SECRET = ${SECRET}; process = ${process.env.SERVICE_SECRET}`)
-		return SECRET === process.env.SERVICE_SECRET
+		if (SECRET === process.env.SERVICE_SECRET) {
+			return true
+		}
+		/*
+		 * 校验不通过写入日志
+		 * */
+		const writeContent = `访问被拒绝,SECRET秘钥不通过\nHost:  ${header.host},  URL:  ${request.url},  Method:  ${request.method}\nuserAgent:  ${userAgent}`
+		Logger.write("warning", writeContent)
 	}
 }
