fda6af74c1413adbbabd3ddc47d2cdebde290542
colin <lukunlin@czb365.com>
2022-01-17 16:27:31 +0800

    commit: 实例化放在类里~

--- src/guards/permission.guard.ts
+++ src/guards/permission.guard.ts
@@ -2,15 +2,14 @@ import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
 import { IncomingMessage } from "http"
 import { LoggerService } from "../logger/logger.service"
 
-// 实例化一些场外对象
-const Logger = new LoggerService()
-
 /**
  * 强制校验SECRET的有效性
  */
 @Injectable()
 export class checkSecretGuard implements CanActivate {
 	constructor() {}
+
+	logger = new LoggerService()
 
 	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const request = context.switchToHttp().getRequest<IncomingMessage>()
@@ -24,6 +23,6 @@ export class checkSecretGuard implements CanActivate {
 		 * 校验不通过写入日志
 		 * */
 		const writeContent = `访问被拒绝,SECRET秘钥不通过\nHost:  ${header.host},  URL:  ${request.url},  Method:  ${request.method}\nuserAgent:  ${userAgent}`
-		Logger.write("warning", writeContent)
+		this.logger.write("warning", writeContent)
 	}
 }
