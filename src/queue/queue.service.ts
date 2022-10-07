b445779855508a5a00bb2e3994fe321069d95778
Lukunlin <bboykungu@qq.com>
2022-10-07 14:14:38 +0800

    feat: 在完成任务的时候删除队列里的缓存

--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from "@nestjs/common"
 import { Queue } from "bull"
+import type { JobOptions } from "bull/index"
 import { InjectQueue } from "@nestjs/bull"
+
+export interface IOptions extends JobOptions {
+	[key: string]: any
+}
 
 @Injectable()
 export class NoticeService {
 	constructor(@InjectQueue("noticeMsg") private queueService: Queue) {}
 
-	public pushNoticeMsg(Msg: string, options: any = {}) {
-		this.queueService.add("noticeMessage", Msg, options)
+	public pushNoticeMsg(Msg: string, options: IOptions = { removeOnComplete: true }) {
+		const opt = Object.assign({ removeOnComplete: true }, options)
+		this.queueService.add("noticeMessage", Msg, opt)
 	}
 
-	public pushNoticeForCzb(Msg: string, options: any = {}) {
+	public pushNoticeForCzb(Msg: string, options: IOptions = { removeOnComplete: true }) {
+		const opt = Object.assign({ removeOnComplete: true }, options)
 		this.queueService.add("noticeForCzb", Msg, options)
 	}
 
-	public pushMarkdownForCzb(markdownContent: string, options: any = {}) {
+	public pushMarkdownForCzb(markdownContent: string, options: IOptions = { removeOnComplete: true }) {
+		const opt = Object.assign({ removeOnComplete: true }, options)
 		this.queueService.add("markdownForCzb", markdownContent, options)
 	}
 }
