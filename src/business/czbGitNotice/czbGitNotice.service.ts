f716be054ef86b5eeaf0b0694c12f0f47471ef9d
colin <lukunlin@czb365.com>
2023-03-16 22:13:13 +0800

    hotfix：修复可能拿到是false不能再去向下取

--- src/business/czbGitNotice/czbGitNotice.service.ts
+++ src/business/czbGitNotice/czbGitNotice.service.ts
@@ -406,7 +406,7 @@ export class CzbGitNoticeService {
 				const updatedResult = await this.pushWeigeTableUpdated(body)
 				// 再推送一个通知到企业微信报告commitId
 				const isDoneInterface = (data): data is IPushWeigeTableUpdated => {
-					return data.done !== undefined
+					return data && data.done
 				}
 				let noticeSub = `最后的CommitId： ${body.commitID}\n`
 				let noticeMembers: string[] = []
